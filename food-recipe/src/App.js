96e02189a1942afb122641296323bd5bf2a0b423
Astro Whang <astro.whang@bluevolt.com>
2019-07-01 16:19:59 -0700

    added api keys and hid them...hopefully

--- food-recipe/src/App.js
+++ food-recipe/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import './App.css';
+import { config } from './config';
 
 function App() {
+
+  const APP_ID = config.APP_ID;
+  const APP_KEY = config.APP_KEY;
+  const example_request = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  console.log(example_request);
+
   return (
-    <div>Hello!</div>
+    <div className="App">
+      <form action="" className="search-form">
+        <input type="text" className="search-bar"/>
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
   );
 }
 
---
+++ food-recipe/src/config.js
@@ -0,0 +1,4 @@
+export const config = {
+    APP_ID : '1c00e299',
+    APP_KEY : '4b6d8be8e0a715dbc1b047e51742cdd5',
+}
